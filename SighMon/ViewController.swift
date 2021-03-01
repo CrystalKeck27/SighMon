6b69f8d9622ede9a592418886567287365114552
Aiden <meaneea@gmail.com>
2021-02-28 18:20:25 -0600

    Initial Commit

---
+++ SighMon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SighMon
+//
+//  Created by Tiger Coder on 2/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
